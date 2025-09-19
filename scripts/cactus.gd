c2d9ba28dc410ddeffcdeb159164fbc97bcd240b
octoscorp <97484622+octoscorp@users.noreply.github.com>
2025-09-19 16:06:48 +1200

    cactus.gd: stop moving if target leaves range

--- scripts/cactus.gd
+++ scripts/cactus.gd
@@ -2,6 +2,10 @@ extends "res://scripts/enemy.gd"
 
 var able_to_move = false
 
+func _ready():
+	# TODO: remove assumption that this will always spawn outside range of target (when enemies/projectiles become targets this will not necessarily be true)
+	pass
+
 func _physics_process(delta: float) -> void:
 	if able_to_move:
 		super._physics_process(delta)
@@ -13,7 +17,9 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	able_to_move = true
 
 func _on_range_area_entered(area: Area2D) -> void:
-	print(area.to_string())
-	print("target: %s" % self.target.to_string())
 	if area == self.target:
 		able_to_move = true
+
+func _on_range_area_exited(area: Area2D) -> void:
+	if area == self.target:
+		able_to_move = false
